a78016c6282e4e98059ad09837542deba0741f57
CAPTAIN1947 <sayantandebin@gmail.com>
2023-05-17 05:22:36 +0530

    added Android.mk

---
+++ Android.mk
@@ -0,0 +1,16 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := supernova
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/src
+LOCAL_CFLAGS :=
+
+LOCAL_SRC_FILES := $(LOCAL_PATH)/src/engine.cpp
+
+LOCAL_SHARED_LIBRARIES := SDL2 SDL2_image SDL2_mixer SDL2_ttf SDL2_net
+
+LOCAL_EXPORT_C_INCLUDES += $(LOCAL_C_INCLUDES)/src
+
+include $(BUILD_SHARED_LIBRARY)
