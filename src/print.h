ed44e7b525e212f5f9ca6a59dbb4f76cd4fe5168
captain1947 <sayantandebin@gmail.com>
2024-01-04 22:18:24 +0530

    new header print.h with python style print

---
+++ src/print.h
@@ -0,0 +1,35 @@
+#ifndef SUPERNOVA_PRINT_H
+#define SUPERNOVA_PRINT_H
+
+
+#include <iostream>
+
+
+
+// Structs
+struct PrintArgs {
+    std::string sep = " ", end = "\n";
+};
+
+
+
+// Functions
+template <typename Arg, typename... Args>
+inline void print(const PrintArgs &print_args, Arg&& arg, Args&&... args) {
+    std::cout << std::forward<Arg>(arg);
+    ((std::cout << print_args.sep << std::forward<Args>(args)), ...);
+    std::cout << print_args.end;
+}
+
+template <typename Arg, typename... Args>
+inline void print(Arg&& arg, Args&&... args) {
+    std::cout << std::forward<Arg>(arg);
+    ((std::cout << ' ' << std::forward<Args>(args)), ...);
+    std::cout << '\n';
+}
+
+inline void print() {
+    std::cout << std::endl;
+}
+
+#endif /* SUPERNOVA_PRINT_H */
