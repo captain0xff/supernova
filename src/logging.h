55407e92468f704603cd293146c4078906c63b28
captain1947 <sayantandebin@gmail.com>
2024-01-21 21:03:47 +0530

    added format args for the logging methods

--- src/logging.h
+++ src/logging.h
@@ -9,6 +9,13 @@
 
 
 
+// Structs
+struct LogArgs {
+	std::string sep = " ", pre = "";
+};
+
+
+
 // Functions
 template <typename Arg, typename... Args>
 std::string log_to_string(Arg&& arg, Args&&... args) {
@@ -20,8 +27,23 @@ std::string log_to_string(Arg&& arg, Args&&... args) {
 }
 
 template <typename Arg, typename... Args>
-void log(Arg&& arg, Args&&... args) {
+std::string log_to_string(const LogArgs &log_args, Arg&& arg, Args&&... args) {
+	std::stringstream buffer;
+	buffer << log_args.pre;
+	buffer << std::forward<Arg>(arg);
+	((buffer << log_args.sep << std::forward<Args>(args)), ...);
+
+	return buffer.str();
+}
+
+template <typename Arg, typename... Args>
+void log_info(Arg&& arg, Args&&... args) {
 	SDL_Log("%s", log_to_string(arg, args...).c_str());
+}
+
+template <typename Arg, typename... Args>
+void log_info(const LogArgs &log_args, Arg&& arg, Args&&... args) {
+	SDL_Log("%s", log_to_string(log_args, arg, args...).c_str());
 }
 
 template <typename Arg, typename... Args>
@@ -30,8 +52,18 @@ void log_error(Arg&& arg, Args&&... args) {
 }
 
 template <typename Arg, typename... Args>
+void log_error(const LogArgs &log_args, Arg&& arg, Args&&... args) {
+	SDL_LogError(0, "%s", log_to_string(log_args, arg, args...).c_str());
+}
+
+template <typename Arg, typename... Args>
 void log_warn(Arg&& arg, Args&&... args) {
 	SDL_LogWarn(0, "%s", log_to_string(arg, args...).c_str());
+}
+
+template <typename Arg, typename... Args>
+void log_warn(const LogArgs &log_args, Arg&& arg, Args&&... args) {
+	SDL_LogWarn(0, "%s", log_to_string(log_args, arg, args...).c_str());
 }
 
 #endif /* SUPERNOVA_LOGGING_H */
